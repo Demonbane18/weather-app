567961a666654fd38a04356e0faa04fc2e68ff9b
Demonbane18 <butterncreamz@gmail.com>
2024-02-10 17:23:54 +0800

    updated file structure and added tabs

---
+++ weather-app/app/(tabs)/_layout.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Entypo, AntDesign } from '@expo/vector-icons';
+import { Link, Tabs } from 'expo-router';
+import { Pressable } from 'react-native';
+
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+function EntypoBarIcon(props: {
+  name: React.ComponentProps<typeof Entypo>['name'];
+  color: string;
+}) {
+  return <Entypo size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+function AntDesignIcon(props: {
+  name: React.ComponentProps<typeof AntDesign>['name'];
+  color: string;
+}) {
+  return <AntDesign size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={Colors[colorScheme ?? 'light'].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="nowcast"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => (
+            <EntypoBarIcon name="location" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="forecast"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => (
+            <AntDesignIcon name="clockcircle" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: '',
+          tabBarIcon: ({ color }) => <TabBarIcon name="gear" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
